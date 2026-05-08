deb48d959a0d16f74e2bf2f6a103cc73d9749ca1
AldoR2 <tokoumur@gmail.com>
2026-05-08 22:29:16 +0700

    fix: fixing model from json after some changes in databases

--- lib/models/jadwal_model.dart
+++ lib/models/jadwal_model.dart
@@ -25,8 +25,8 @@ class JadwalModelApi {
 
   factory JadwalModelApi.fromJson(Map<String, dynamic> json) {
     return JadwalModelApi(
-        presensiId: json['presensis_id'],
-        presensisId: json['presensi_id'],
+        presensiId: json['presensi_id'].toString(),
+        presensisId: json['presensis_id'],
         waktu: json['durasi_presensi'],
         mataKuliah: json['nama_matkul'],
         lokasi: json['nama_ruangan'],
