4732c5ee9da9e4eb884adc7c205ac078cfc6ec65
AldoR2 <tokoumur@gmail.com>
2026-05-30 23:12:39 +0700

    debug: test java 17 again

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -12,19 +11,16 @@ android {
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true  // Aktifkan desugaring
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "JavaVersion.VERSION_11.toString()"
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.stipres"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 28
         targetSdk = 33
         versionCode = flutter.versionCode
@@ -35,15 +31,12 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 dependencies {
-    // Tambahkan dependency desugaring:
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
